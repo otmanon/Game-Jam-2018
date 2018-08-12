dbb2ac6d3745d8f058473f0b8df820b11142be4f
Thomas Fix <thomas.fix@mail.mcgill.ca>
2018-08-12 00:08:30 -0400

    Created the level generation scripts

---
+++ Assets/Scripts/ChunkManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour {
+	[Range(1.0f, 2.0f)]
+	public float initialDifficulty = 1.0f;
+	[Range(1.0f, 2.0f)]
+	public float difficultyModifier = 1.15f;
+	[Range(2, 10)]
+	public int chunkBufferSize = 5;
+	public float maxY = 10.0f;
+	public float moveSpeed = 0.02f;
+	
+	private GameObject go;
+	private LevelGenerator lg;
+	private List<GameObject> chunkList;
+	private float currentDifficulty;
+	private Vector2 chunkDimensions;
+	private int nextChunkID = 1;
+
+	void Start () {
+		// instantiate references and attributes
+		go = this.gameObject;
+		lg = GameObject.FindGameObjectWithTag("Level Generator").GetComponent<LevelGenerator>();
+		chunkList = new List<GameObject>();
+		chunkDimensions = lg.GetDimensions();
+		currentDifficulty = initialDifficulty;
+	}
+
+	void Awake() {
+		// initial buffered chunk creation
+		for (int i = 0; i < chunkBufferSize; i++) {
+			chunkList.Add(lg.GenerateChunk(currentDifficulty, nextChunkID++));
+			currentDifficulty *= difficultyModifier;
+		}
+
+		// initial buffered chunk placement
+		for (int i = 0; i < chunkBufferSize; i++) {
+			// TODO
+		}
+	}
+	
+	void FixedUpdate () {
+		// TODO
+		
+		// move the chunks up at a constant rate
+		foreach (GameObject chunk in chunkList) {
+			Vector3 chunkPosition = chunk.transform.position;
+			chunkPosition.y = chunkPosition.y + moveSpeed;
+			chunk.transform.position = chunkPosition;
+		}
+
+		// if they end up outside of the camera, unload and load a new chunk
+
+	}
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+	public int chunkWidth;
+	public int chunkHeight;
+	public GameObject floorTile;
+	public GameObject enemy;
+	public float maxDrop;
+	public float minPlatformLength;
+	public float maxPlatformLength;
+	public float minSeparation;
+
+	// given the current difficulty, and the chunk size
+	// return a gameobject holding a new chunk
+	public GameObject GenerateChunk(float currentDifficulty, int chunkID) {
+		int[,] graph = new int[chunkWidth, chunkHeight];
+
+		// pass 1: platform placement
+		for (int i = 0; i < chunkWidth; i++) {
+			int platformLength = 0;
+			for (int j = 0; j < chunkHeight; j++) {
+				// determine whether we have blank space or a platform
+				// TODO
+				if (j == chunkHeight - 1 || j == 0)
+					graph[i,j] = 0;
+				else
+					graph[i,j] = 1;
+			}
+		}
+
+		// pass 2: enemy and trap placement
+		for (int i = 0; i < chunkWidth; i++) {
+			for (int j = 0; j < chunkHeight; j++) {
+				// place an enemy, or a trap
+				// TODO
+				if (graph[i,j] == 0 && Random.value > 1.0f / currentDifficulty) {
+					// place enemy
+					graph[i,j] = 2;
+				}
+			}
+		}
+
+		// generate gameobject representation of the chunk
+
+		string goName = "chunk" + chunkID;
+		GameObject result = new GameObject(goName);
+		result.SetActive(false);
+
+		for (int i = 0; i < chunkWidth; i++) {
+			for (int j = 0; j < chunkHeight; j++) {
+				if (graph[i,j] == 1) {
+					GameObject obstacle = GameObject.Instantiate(floorTile, result.transform);
+					obstacle.transform.localPosition = new Vector3(i - chunkWidth / 2, chunkHeight / 2 - j, 0);
+				} else if (graph[i,j] == 2) {
+					// GameObject enemy = GameObject.Instantiate();
+				}
+			}
+		}
+
+		return result;
+	}
+
+	// return the chunk size
+	public Vector2 GetDimensions() {
+		return new Vector2(chunkWidth, chunkHeight);
+	}
+}
