97d30a1e3d9a40140beb27abfaa56ccd854bff97
otmanon <31936863+otmanon@users.noreply.github.com>
2018-08-12 14:22:52 -0400

    Revert "Revert "SmolChanges""
    This reverts commit 6ea32648453f7547faf183864cc67e186fcdef86.

---
+++ Assets/TestScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyesLook : MonoBehaviour
+{
+
+    public Transform reference;
+    public float factor = 0.25f;
+    public float limit = 0.08f;
+
+    private Vector3 center;
+
+    void Start()
+    {
+        center = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0.0f;
+        Vector3 dir = (pos - reference.position) * factor;
+        dir = Vector3.ClampMagnitude(dir, limit);
+        Vector3 tt = center + dir;
+        transform.position = center + dir;
+    }
+}
