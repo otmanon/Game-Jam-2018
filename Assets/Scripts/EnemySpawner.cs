a9e35c52f72c180022ea801147200010edec5d75
otmanon <31936863+otmanon@users.noreply.github.com>
2018-08-12 17:32:41 -0400

    Added Sounds

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
-    public GameObject enemy_1;
-    public GameObject enemy_2;
-    public GameObject enemy_3;
-    public GameObject enemy_4;
-    public GameObject enemy_5;
-    public GameObject enemy_6;
-    public GameObject enemy_7;
+    public GameObject cake;
+    public GameObject bear;
+    public GameObject Billy;
+    public GameObject Flower;
+    public GameObject coach;
+    public GameObject football;
+    public GameObject Woodrow;
 
     // Use this for initialization
     void Start () {
@@ -31,30 +31,38 @@ public class EnemySpawner : MonoBehaviour {
 	}
     void Spawn_1 ()
     {
-        GameObject clone = (GameObject)Instantiate(enemy_1);
+       
+        GameObject clone = (GameObject)Instantiate(cake);
+        SoundManagerScript.PlaySound("cake");
     }
     void Spawn_2()
     {
-        GameObject clone = (GameObject)Instantiate(enemy_2);
+        GameObject clone = (GameObject)Instantiate(bear);
+        SoundManagerScript.PlaySound("bear");
     }
     void Spawn_3()
     {
-        GameObject clone = (GameObject)Instantiate(enemy_3);
+        GameObject clone = (GameObject)Instantiate(Billy);
+        SoundManagerScript.PlaySound("billy");
     }
     void Spawn_4()
     {
-        GameObject clone = (GameObject)Instantiate(enemy_4);
+        GameObject clone = (GameObject)Instantiate(Flower);
+        SoundManagerScript.PlaySound("flower");
     }
     void Spawn_5()
     {
-        GameObject clone = (GameObject)Instantiate(enemy_5);
+        GameObject clone = (GameObject)Instantiate(coach);
+        SoundManagerScript.PlaySound("coach");
     }
     void Spawn_6()
     {
-        GameObject clone = (GameObject)Instantiate(enemy_6);
+        GameObject clone = (GameObject)Instantiate(football);
+        SoundManagerScript.PlaySound("football");
     }
     void Spawn_7()
     {
-        GameObject clone = (GameObject)Instantiate(enemy_7);
+        GameObject clone = (GameObject)Instantiate(Woodrow);
+        SoundManagerScript.PlaySound("woodrow");
     }
 }
--- Assets/Scripts/SoundManagerScript.cs
+++ Assets/Scripts/SoundManagerScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SoundManagerScript : MonoBehaviour {
     public static AudioClip Bye_Bye_Baby, Eerie_Background_Shit, LambChops, Spannenlanger_Hansel, SpookyTwinkle;
     public static AudioSource audioSrc;
+    public static AudioClip cake, bear, Billy, Flower, coach, football, Woodrow;
+    public static AudioClip teleportSound, throwSound;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +15,17 @@ public class SoundManagerScript : MonoBehaviour {
         LambChops = Resources.Load<AudioClip>("LambChops");
         Spannenlanger_Hansel = Resources.Load<AudioClip>("Spannelanger_Hansel (1)");
         SpookyTwinkle = Resources.Load<AudioClip>("Spooky Twinkle (1)");
+
+        cake = Resources.Load<AudioClip>("Cake");
+        bear = Resources.Load<AudioClip>("Bear");
+        Billy = Resources.Load<AudioClip>("Billy");
+        coach = Resources.Load<AudioClip>("CoachCreul");
+        football= Resources.Load<AudioClip>("Football");
+        Flower = Resources.Load<AudioClip>("Flowers");
+        Woodrow = Resources.Load<AudioClip>("Woodrow");
+
+        throwSound = Resources.Load<AudioClip>("Throw");
+        teleportSound = Resources.Load<AudioClip>("teleportSound");
 
         audioSrc = GetComponent<AudioSource>();
 
@@ -28,15 +41,43 @@ public class SoundManagerScript : MonoBehaviour {
 		
 	}
 
-    public static void PlaySound ()
+    public static void PlaySound (string clip)
     {
-        while (true)
+        if (clip == "cake")
         {
-            audioSrc.PlayOneShot (Bye_Bye_Baby);
-            audioSrc.PlayOneShot(Eerie_Background_Shit);
-            audioSrc.PlayOneShot(LambChops);
-            audioSrc.PlayOneShot(Spannenlanger_Hansel);
-            audioSrc.PlayOneShot(SpookyTwinkle);
+            audioSrc.PlayOneShot (cake);
+        }
+        if (clip == "bear")
+        {
+            audioSrc.PlayOneShot(cake);
+        }
+        if (clip == "billy")
+        {
+            audioSrc.PlayOneShot(Billy);
+        }
+        if (clip == "coach")
+        {
+            audioSrc.PlayOneShot(coach);
+        }
+        if (clip == "football")
+        {
+            audioSrc.PlayOneShot(football);
+        }
+        if (clip == "flower")
+        {
+            audioSrc.PlayOneShot(Flower);
+        }
+        if (clip == "woodrow")
+        {
+            audioSrc.PlayOneShot(Woodrow);
+        }
+        if (clip == "teleport")
+        {
+            audioSrc.PlayOneShot(teleportSound);
+        }
+        if (clip == "throw")
+        {
+            audioSrc.PlayOneShot(throwSound);
         }
     }
 }
